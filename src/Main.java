b6d3ca9d357e66f69671412eed6e371439904c2c
dollemika <dollemika@mail.ru>
2024-04-01 12:17:41 +0300

    testing commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        testCar();
         testWizard();
 
-    }
-    public static void testCar(){
-        Car car1 = new Car();
-        car1.setBrand("Бибика");
-        car1.setV(0);
-        Car car2 = new Car("Лада",60);
-        car1.addSpeed(50);
-        System.out.println("Скорость "+car1.getBrand()+" = "+car1.getV());
-        car1.lowerSpeed(45);
-        System.out.println("Скорость "+car1.getBrand()+" = "+car1.getV());
-        System.out.println(car2.getBrand()+" быстрее на = "+(car2.getV()-car1.getV()));
     }
     public static void testWizard(){
         Wizard wizard1 = new Wizard("Мерлин", 100);
